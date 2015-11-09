9b6b2d60192045234baae56f0977e6f81e1aa2e2
gges5110 <gges5110@gmail.com>
2015-11-09 16:06:55 -0600

    include library and can fully utilize calendar api now

--- main.py
+++ main.py
@@ -15,11 +15,48 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+import datetime
+from apiclient.discovery import build
+from oauth2client.appengine import OAuth2DecoratorFromClientSecrets
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions = ['jinja2.ext.autoescape'],
+    autoescape = True
+    )
+
+decorator = OAuth2DecoratorFromClientSecrets(
+  os.path.join(os.path.dirname(__file__), 'client_secret.json'),
+  'https://www.googleapis.com/auth/calendar.readonly')
+
+service = build('calendar', 'v3')
 
 class MainHandler(webapp2.RequestHandler):
+    @decorator.oauth_required
     def get(self):
-        self.response.write('Hello world!')
+        # Get the authorized Http object created by the decorator.
+        http = decorator.http()
+        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+        # Call the service using the authorized Http object.
+        eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute(http=http)
+        events = eventsResult.get('items', [])
+        if not events:
+            event_str = "no up coming events"
+        else:
+            event = events[0]
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            event_str = start + event['summary']
+        template_values = {
+            'event' : event_str
+        }
+        template = JINJA_ENVIRONMENT.get_template('/templates/login.html')
+        self.response.write(template.render( template_values ))        
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    (decorator.callback_path, decorator.callback_handler())
 ], debug=True)
